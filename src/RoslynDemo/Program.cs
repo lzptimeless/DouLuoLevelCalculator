ba71e5076540ea906a90c74d7d209dfa7c0ee02b
zhongping.ling <zhongping.ling@razer.com>
2024-05-21 18:42:45 +0800

    研究脚本

--- src/RoslynDemo/Program.cs
+++ src/RoslynDemo/Program.cs
@@ -4,6 +4,7 @@ using RoslynDemo;
 using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
+var global = new ScriptGlobal();
 
 Stopwatch watch = Stopwatch.StartNew();
 _ = ScriptHelper.ExecuteScriptAsync("Console.WriteLine(\"Script 1\")").Result;
@@ -11,11 +12,11 @@ watch.Stop();
 Console.WriteLine($"During: {watch.Elapsed}");
 
 watch = Stopwatch.StartNew();
-_ = ScriptHelper.ExecuteScriptAsync("Console.WriteLine(\"Script 2\" + Name)", new ScriptGlobal()).Result;
+_ = ScriptHelper.ExecuteScriptAsync("Console.WriteLine(\"Script 2\" + Name); Name = Name + \"3\";", global).Result;
 watch.Stop();
 Console.WriteLine($"During: {watch.Elapsed}");
 
 watch = Stopwatch.StartNew();
-_ = ScriptHelper.ExecuteScriptAsync("Console.WriteLine(\"Script 3\")").Result;
+var result = ScriptHelper.ExecuteScriptAsync("Console.WriteLine(\"Script 3\" + Name); return Name + \"4\";", global).Result;
 watch.Stop();
-Console.WriteLine($"During: {watch.Elapsed}");
+Console.WriteLine($"During: {watch.Elapsed}, {result}");
